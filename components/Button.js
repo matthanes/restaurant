37162b2e2c181b583aa689ea001ac0646a2132c7
matthanes <hanes.matt@gmail.com>
2022-07-07 23:28:24 -0400

    remove unnecessary hover

--- components/Button.js
+++ components/Button.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 const Button = ({href, text}) => {
   return (
     <Link href={href}>
-      <a className="lg:inline-flex lg:w-auto lg:ml-8 px-3 py-2 rounded text-white font-bold items-center justify-center w-fit bg-rose-600  lg:bg-rose-600 hover:bg-rose-800 transition duration-500">
+      <a className="lg:inline-flex lg:w-auto lg:ml-8 px-3 py-2 rounded text-white font-bold items-center justify-center w-fit bg-rose-600 hover:bg-rose-800 transition duration-500">
         {text}
       </a>
     </Link>
