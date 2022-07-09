d92c96a941eae1fef0e4ddb87617c24193319c86
matthanes <hanes.matt@gmail.com>
2022-07-08 22:11:51 -0400

    Change logo to initials

--- components/Logo.js
+++ components/Logo.js
@@ -2,8 +2,8 @@ import Link from "next/link";
 
 const Logo = () => (
   <Link href="/">
-  <a className="flex items-center text-4xl text-white font-bold tracking-wider">
-    <span className="ml-2 font-montez">Taste of India</span>
+  <a className="flex items-center text-4xl text-white font-bold tracking-[.25em]">
+    <span className="ml-2 font-montez">TI</span>
   </a>
 </Link>
 )
