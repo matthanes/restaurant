0b829e0b12532c93f74cc1d1db64632eb2388e8e
matthanes <hanes.matt@gmail.com>
2022-07-08 22:12:15 -0400

    Homepage changes

--- pages/index.js
+++ pages/index.js
@@ -18,12 +18,13 @@ export default function Home() {
           layout="raw"
           width={1920}
           height={1080}
-          alt="A night sky"
+          alt=""
           className="h-screen lg:h-fullpage object-cover mx-auto"
+          aria-label="presentation"
         />
         <div className="absolute inset-0 w-full h-full min-h-screen lg:min-h-0 bg-slate-900 bg-opacity-80 flex justify-center items-center">
           <section className="text-white">
-            <h1 className="text-8xl text-center font-montez my-8 px-4">Taste of India</h1>
+            <h1 className="text-8xl text-center font-montez mb-8 px-4">Taste of India</h1>
             <p className="text-3xl text-center font-montaga px-4">Your home for authentic Indian Cuisine.</p>
           </section>
         </div>
